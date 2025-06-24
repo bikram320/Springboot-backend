2b53ccc4bd0ee059e7c32f6c90c7a5f4498ea038
Bikram Bishwokarma <bkbikram727@gmail.com>
2025-06-24 20:57:10 +0545

    Creating Dynamic routes and using it in postman

--- src/main/java/com/example/spring_api_starter/controllers/UserController.java
+++ src/main/java/com/example/spring_api_starter/controllers/UserController.java
@@ -4,19 +4,25 @@ import com.example.spring_api_starter.entities.User;
 import com.example.spring_api_starter.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Iterator;
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public Iterator<User> getUsers() {
         return userRepository.findAll().iterator();
+    }
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
