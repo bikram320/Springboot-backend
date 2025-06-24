0b36adad89201314de18ea47446f5b4c95237c22
Bikram Bishwokarma <bkbikram727@gmail.com>
2025-06-24 19:30:28 +0545

    understanding building an api

--- src/main/java/com/example/spring_api_starter/controllers/HomeController.java
+++ src/main/java/com/example/spring_api_starter/controllers/HomeController.java
@@ -1,17 +1,16 @@
 package com.example.spring_api_starter.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
+    public String index(Model model) {
+        model.addAttribute("name", "Bikram");
+        return "index";
     }
-    @RequestMapping("/hello")
-    public String sayHello() {
-        return "index.html";
-    }
+
 }
---
+++ src/main/java/com/example/spring_api_starter/entities/Message.java
@@ -0,0 +1,10 @@
+package com.example.spring_api_starter.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Message {
+    private String message;
+}
