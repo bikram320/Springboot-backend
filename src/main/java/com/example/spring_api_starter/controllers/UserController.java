021d4da4b6484f42894bde420a27921d2e498390
Bikram Bishwokarma <bkbikram727@gmail.com>
2025-06-25 17:40:05 +0545

    understanding dtos

--- src/main/java/com/example/spring_api_starter/controllers/UserController.java
+++ src/main/java/com/example/spring_api_starter/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.example.spring_api_starter.controllers;
 
+import com.example.spring_api_starter.dtos.UserDto;
 import com.example.spring_api_starter.entities.User;
 import com.example.spring_api_starter.repositories.UserRepository;
 import lombok.AllArgsConstructor;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Iterator;
 
 @RestController
 @AllArgsConstructor
@@ -20,15 +20,21 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping
-    public Iterator<User> getUsers() {
-        return userRepository.findAll().iterator();
+    public Iterable<UserDto> getUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user->
+                    new UserDto(user.getId(), user.getName(), user.getEmail()))
+                .toList();
+
     }
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
         var user= userRepository.findById(id).orElse(null);
         if (user == null){
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(user);
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 }
