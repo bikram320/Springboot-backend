2545f7462b7d69cfbb4eb0480a43340c8b04d862
Bikram Bishwokarma <bkbikram727@gmail.com>
2025-06-25 19:28:25 +0545

    understanding extracting query parameter

--- src/main/java/com/example/spring_api_starter/controllers/UserController.java
+++ src/main/java/com/example/spring_api_starter/controllers/UserController.java
@@ -6,12 +6,12 @@ import com.example.spring_api_starter.entities.User;
 import com.example.spring_api_starter.mapper.UserMapper;
 import com.example.spring_api_starter.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 
 @RestController
@@ -23,8 +23,11 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getUsers() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getUsers(@RequestParam(required = false , defaultValue = "" ,name = "sort") String sortBy) {
+
+        if (!Set.of("name","email").contains(sortBy))
+            sortBy = "name";
+        return userRepository.findAll(Sort.by(sortBy))
                 .stream()
                 .map(userMapper::toUserDto)
                 .toList();
--- src/main/java/com/example/spring_api_starter/dtos/UserDto.java
+++ src/main/java/com/example/spring_api_starter/dtos/UserDto.java
@@ -1,9 +1,13 @@
 package com.example.spring_api_starter.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 @AllArgsConstructor
 @Getter
@@ -13,4 +17,5 @@ public class UserDto {
     private long id;
     private String name;
     private String email;
+
 }
