a6eba767766231ae708ac745e3ee3cbea2fb9939
[Bikram Bk] <bkbikram727@gmail.com>
2025-06-28 21:38:22 +0545

    solve all bugs and added updating resources
    bugs are solved and used mapper and create update users

--- src/main/java/com/example/spring_api_starter/mapper/UserMapper.java
+++ src/main/java/com/example/spring_api_starter/mapper/UserMapper.java
@@ -5,10 +5,14 @@ import com.example.spring_api_starter.dtos.UpdateUserRequest;
 import com.example.spring_api_starter.dtos.UserDto;
 import com.example.spring_api_starter.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "email", target = "email")
     UserDto toUserDto(User user);
     User toUser(RegisterUserRequest request);
     void updateUser(UpdateUserRequest request ,@MappingTarget User user);
